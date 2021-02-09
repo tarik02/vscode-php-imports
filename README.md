046ed2c42291da704360a385e8e6c3d52b77005e
Tarik02 <Taras.Fomin@gmail.com>
2021-02-09 16:05:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# vscode-php-imports
+
+PHP imports formatter for Visual Studio Code.
+
+## Commands
+
+* `vscode-php-imports.format`: PHP Imports: Format
+
+## Extension Settings
+
+* `php-imports.formatOnSave`: Automatically format imports before saving PHP file.
+* `php-imports.order`: Order of import sets and empty lines between them. An array of enums with possible values:
+	- `singleUses.class`
+	- `emptyLine`
+	- `singleUses.function`
+	- `singleUses.const`
+	- `emptyLine`
+	- `groupedUses.class`
+	- `emptyLine`
+	- `groupedUses.function`
+	- `groupedUses.const`
+* `php-imports.sort.order`: Order of first-level imports. Possible values: `default` (alphabetical) or `natural` (first longest, alphabetical).
+* `php-imports.sort.nestedOrder`: Order of second-level imports. Possible values: `default` (alphabetical) or `natural` (first longest, alphabetical).
+* `php-imports.print.emptyLinesAfterImports`: Count of empty lines between last import and the declaration after it.
+* `php-imports.psr12.enable`: Enable PSR-12 imports formatting.
+* `php-imports.psr12.isolateModifiers`: Whether to put different modifiers (without modifier, const and function) to different groups.
+* `php-imports.psr12.minNestedGroupNestedUsesCount`: Minimum count of nested uses to make a nested group with them.
+* `php-imports.psr12.minNestedGroupUsesCount`: Mimimum count of uses to make a nested group with them.
+* `php-imports.psr12.minGroupUsesCount`: Mimimum count of uses of same namespace to make a group with them.
+* `php-imports.custom.enable`: Enable custom imports formatting
+* `php-imports.custom.isolateModifiers`: Whether to put different modifiers (without modifier, const and function) to different groups.
+* `php-imports.custom.include`: A list of namespaces that should always be grouped. Can be used with wildcards (*) and double wildcards (**).
+* `php-imports.custom.exclude`: A list of namespaces that should never be grouped.
+
+## Release Notes
+
+### 0.1.0
+
+Initial release
+
+## License
+
+The project is released under the MIT license. Read the [license](https://github.com/Tarik02/vscode-php-imports/blob/master/LICENSE) for more information.
---
+++ src/extension.ts
@@ -0,0 +1,143 @@
+import * as PhpImports from 'php-imports'
+import * as vscode from 'vscode'
+
+
+export function activate(context: vscode.ExtensionContext): void {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vscode-php-imports.format', async () => {
+			const editor = vscode.window.activeTextEditor
+
+			if (!editor) {
+				return
+			}
+
+			await formatImportsInEditorWithFeedback(editor)
+		}),
+
+		vscode.workspace.onWillSaveTextDocument(event => {
+			if (!(
+				event.document.languageId === 'php' &&
+				vscode.workspace.getConfiguration('php-imports').get('formatOnSave')
+			)) {
+				return
+			}
+
+			event.waitUntil((async () => {
+				const edit = await prepareEditForDocument(event.document, '  ')
+
+				if (!edit) {
+					return []
+				}
+
+				return [
+					vscode.TextEdit.replace(
+						new vscode.Range(
+							event.document.positionAt(edit.start),
+							event.document.positionAt(edit.end),
+						),
+						edit.replacement,
+					),
+				]
+			})())
+		}),
+	)
+}
+
+export function deactivate(): void {
+	//
+}
+
+async function formatImportsInEditorWithFeedback(editor: vscode.TextEditor): Promise<void> {
+	try {
+		if (await formatImportsInEditor(editor)) {
+			vscode.window.showInformationMessage('Imports successfully formatted')
+		} else {
+			vscode.window.showInformationMessage('Imports are already formatted')
+		}
+	} catch (err) {
+		console.error(err)
+
+		vscode.window.showErrorMessage('Failed to format imports')
+	}
+}
+
+async function formatImportsInEditor(editor: vscode.TextEditor): Promise<boolean> {
+	const indent = editor.options.insertSpaces
+		? ' '.repeat(editor.options.tabSize as number)
+		: '\t'
+
+	const edit = await prepareEditForDocument(editor.document, indent)
+
+	if (!edit) {
+		return false
+	}
+
+	return await editor.edit(builder => {
+		builder.replace(
+			new vscode.Range(
+				editor.document.positionAt(edit.start),
+				editor.document.positionAt(edit.end),
+			),
+			edit.replacement,
+		)
+	})
+}
+
+async function prepareEditForDocument(editorDocument: vscode.TextDocument, indent: string): Promise<{ start: number, end: number, replacement: string } | undefined> {
+	const configuration = vscode.workspace.getConfiguration('php-imports')
+	const text = editorDocument.getText()
+
+	const document = PhpImports.Grammar.fromSource(text)
+	const flat = PhpImports.Flat.fromGrammar(document.uses)
+	const tree = PhpImports.Tree.fromFlat(flat)
+
+	const grouped = PhpImports.Group.create()
+
+	if (configuration.get('psr12.enable')) {
+		PhpImports.Group.collectPsr12(tree, grouped, {
+			minNestedGroupNestedUsesCount: configuration.get('psr12.minNestedGroupNestedUsesCount'),
+			minNestedGroupUsesCount: configuration.get('psr12.minNestedGroupUsesCount'),
+			minGroupUsesCount: configuration.get('psr12.minGroupUsesCount'),
+			isolateModifiers: configuration.get('psr12.isolateModifiers'),
+		})
+	}
+
+	if (configuration.get('custom.enable')) {
+		PhpImports.Group.collectCustom(tree, grouped, {
+			include: configuration.get('custom.include'),
+			exclude: configuration.get('custom.exclude'),
+		})
+	}
+
+	PhpImports.Group.collectAllToSingles(tree, grouped)
+
+	PhpImports.Group.sort(grouped, {
+		order: configuration.get('sort.order'),
+		nestedOrder: configuration.get('sort.nestedOrder'),
+	})
+
+	const printed = PhpImports.Print.print(grouped, {
+		order: configuration.get('order'),
+		indent,
+	})
+
+	const prefix = text.substring(0, document.uses.location.start.offset)
+	const suffix = text.substring(document.uses.location.end.offset)
+
+	const startOffset = document.uses.location.start.offset - (prefix.length - prefix.trimEnd().length)
+	const endOffset = document.uses.location.end.offset + (suffix.length - suffix.trimStart().length)
+
+	const emptyLinesAfterImports = configuration.get<number>('print.emptyLinesAfterImports', 1)
+
+	const replacement = `\n\n${printed}\n` + '\n'.repeat(emptyLinesAfterImports)
+
+	if (text.substring(startOffset, endOffset) === replacement) {
+		return undefined
+	}
+
+	return {
+		start: startOffset,
+		end: endOffset,
+		replacement,
+	}
+}
